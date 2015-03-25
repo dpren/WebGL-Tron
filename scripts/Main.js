c7c53b272a65fe11df518787aea2d850360ae096
dpren <prenticedarren@gmail.com>
2015-03-25 18:21:48 -0500

    wall fix, gradual brake, constrain speed

--- scripts/Main.js
+++ scripts/Main.js
@@ -11,7 +11,8 @@ var halfPi = Math.PI/2;
 	var scene = new THREE.Scene();
 
 	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 1, 1000 );
-		camera.position.set(50,20,50);
+		camera.position.set(-30,30,50);
+		//camera.position.set(-10,10,0);
 		camera.rotation.x = -0.7;
 
 
@@ -19,7 +20,7 @@ var halfPi = Math.PI/2;
 		cameraControls.maxDistance = 400;
 
 
-	var resizeWindow = function () {
+	var resizeWindow = function() {
 		camera.aspect = window.innerWidth/window.innerHeight;
 		camera.updateProjectionMatrix();
 		renderer.setSize( window.innerWidth, window.innerHeight );
@@ -31,7 +32,8 @@ var halfPi = Math.PI/2;
 
 
 
-//line grid
+
+/*—–––––––––––line grid—–––––––––––*/
 var size = 60, step = 4;
 var geo = new THREE.Geometry();
 var lineMaterial = new THREE.LineBasicMaterial({ color: 'white' });
@@ -44,49 +46,51 @@ for (var j = -size; j <= size; j += step) {
 var grid = new THREE.Line( geo, lineMaterial, THREE.LinePieces);
 	scene.add(grid);
 
-//plane grid
+// solid plane grid
 // var groundGeometry = new THREE.PlaneBufferGeometry(120,120,30,30);
 // var groundMaterial = new THREE.MeshBasicMaterial({ wireframe: false, color: 0x333333 });
 // var ground = new THREE.Mesh( groundGeometry, groundMaterial );
 // 	ground.rotateX(halfPi);
 // 	scene.add( ground );
+/*–––––––––––––––––––––––––––––––––*/
 
 
 
 
-
-var cubeMaterial = new THREE.MeshLambertMaterial({
+/*—–––––––––––cycle constructor—–––––––––––*/
+var cycleMaterial = new THREE.MeshLambertMaterial({
 	color: 0x00ffbb,
 	transparent: true,
 	opacity: 0.8
 });
 
-var cube = new THREE.Mesh(new THREE.BoxGeometry(4,4,2), cubeMaterial);
+var cube = new THREE.Mesh(new THREE.BoxGeometry(4,4,2), cycleMaterial);
 
 var bcylinder = new THREE.CylinderGeometry( 2, 2, 3, 16 );
-var bwheel = new THREE.Mesh( bcylinder, cubeMaterial );
+var bwheel = new THREE.Mesh( bcylinder, cycleMaterial );
 	bwheel.position.set(-1.5,0,0);
 	bwheel.rotateX(halfPi);
 	scene.add(bwheel);
 
 var fcylinder = new THREE.CylinderGeometry( 0.7, 0.7, .5, 16 );
-var fwheel = new THREE.Mesh( fcylinder, cubeMaterial );
+var fwheel = new THREE.Mesh( fcylinder, cycleMaterial );
 	fwheel.position.set(2,-1.3,0);
 	fwheel.rotateX(halfPi);
 	scene.add(fwheel);
 
-//ye olde light-tractor xD
+// ye olde light-tractor xD
 var lightcycle = new THREE.Object3D();
 	lightcycle.add(cube);
 	lightcycle.add(bwheel);
 	lightcycle.add(fwheel);
 	lightcycle.position.set(0,2,0);
 	scene.add(lightcycle);
+/*–––––––––––––––––––––––––––––––––––––––––*/
 
 
 
 
-
+/*—–––––––––––wall constructor—–––––––––––*/
 var wallMaterial = new THREE.MeshBasicMaterial({
 	color: new THREE.Color( 0x00ffdd ),
 	side: THREE.DoubleSide,
@@ -98,9 +102,9 @@ var wallMaterial = new THREE.MeshBasicMaterial({
 var wallGeometry = new THREE.PlaneBufferGeometry( 1, 4 );
 var m = new THREE.Matrix4();
 	m.makeRotationX(halfPi);
-	m.makeTranslation( 0.5, 2, 0.5 );
+	m.makeTranslation( 0.5, 2, 0 );
 	wallGeometry.applyMatrix( m );
-
+/*––––––––––––––––––––––––––––––––––––––––*/
 
 
 
@@ -116,20 +120,26 @@ scene.add(pointLight2);
 
 
 
-var playerspeed = 0.5;
 
+var paused = true;
+var view = true;
 
 var i = 1;
 var lastDir;
-var lastDirection = new THREE.Vector3( 0, 0, 0 );
+var playerspeed = 0.1;
+
+function constrain(min, max) {
+  return function(n) {
+    return Math.max(min, Math.min(n, max));
+  };
+}
+var constrainSpeed = constrain(0.4, 1);
+
 
 var addTurnCoord = function () {
 	console.log(lightcycle.position);
 };
 
-
-var paused = true;
-var view;
 
 
 var onKeyDown = function(e) {
@@ -137,24 +147,22 @@ var onKeyDown = function(e) {
 		case 70: // left
 		case 68:
 		case 83:
-		case 65: 	addTurnCoord();
-					++i;
+		case 65: 	++i;
+					addTurnCoord();
 					lightcycle.rotateY(halfPi);
 					if(i > 3){i = 0;}
 					break;
 		case 74: // right
 		case 75:
 		case 76:
-		case 186: 	addTurnCoord();
-					--i;
+		case 186: 	--i;
+					addTurnCoord();
 					lightcycle.rotateY(-halfPi);
 					if(i < 0){i = 3;}
 					break;
-
 		case 80: // p
 					paused = !paused;
 					break;
-
 		case 86: // v
 					view = !view;
 					break;
@@ -165,7 +173,8 @@ document.addEventListener('keydown', this.onKeyDown, false);
 
 
 
-var animate = function () {
+
+var animate = function() {
 	
 	requestAnimationFrame(animate);
 
@@ -177,13 +186,15 @@ var animate = function () {
 		GUI.style.visibility="visible";
 	}
 
-	//brake
 	if (keyboard.pressed("space")) {
-		playerspeed = 0.2;
+		// brake
+		playerspeed = constrainSpeed(playerspeed -= 0.05);
 	} else {
-		playerspeed = 0.5;
+		// accel
+		playerspeed = constrainSpeed(playerspeed += 0.05);
 	}
 
+	// camera view
 	if (view === true) {
 		camera.lookAt(lightcycle.position);
 	}
@@ -193,7 +204,9 @@ var animate = function () {
 };
 
 
-var moveStuff = function(turn) {
+
+
+var moveStuff = function() {
 
 	var dirX = [
 		0,
@@ -209,31 +222,21 @@ var moveStuff = function(turn) {
 	];
 
 
-	var lightcycleDirection = new THREE.Vector3( playerspeed, 0, 0 );
-	lightcycleDirection.applyMatrix3( lightcycle.matrix );
-	var turned = !lightcycleDirection.equals( lastDirection );
-
-
-	//if ( turned ) {
 	if (i !== lastDir) {
-		//we have turned, add new wall segment
-		lastDirection = lightcycleDirection.clone();
+		// we have turned, add new wall segment
 		lastDir = i;
-
 	    wall = new THREE.Mesh(wallGeometry, wallMaterial);
 		wall.quaternion.copy(lightcycle.quaternion);
-		//wall.position = lightcycle.position.clone();
 		wall.position.set(lightcycle.position.x,0,lightcycle.position.z);
 		wall.scale.x = 0;
 		scene.add(wall);
 	}
 
-	// move light cycle
+
+	// move light cycle in direction i
 	lightcycle.position.x -= dirX[i];
 	lightcycle.position.z -= dirZ[i];
 
-	//alternate method
-	//lightcycle.position.add( lightcycleDirection );
 
 	// move wall
 	wall.scale.x += playerspeed;
